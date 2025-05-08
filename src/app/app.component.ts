3622a21c772694c9e8e02635197adbd24c2e6123
Ryu <j-na1@hotmail.fr>
2025-05-08 20:52:25 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatIcon],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'card-collection';
+
+  private router = inject(Router);
+
+
+  navigateToLogin(){
+    this.router.navigate(['login']);
+  }
+
+  navigateHome(){
+    this.router.navigate(['home'])
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,60 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './component/home/home/home.component';
+import {PokemonListComponent} from './component/pokemon/pokemon-list/pokemon-list.component';
+import {PokemonDetailsComponent} from './component/pokemon/pokemon-details/pokemon-details/pokemon-details.component';
+import {YugiohListComponent} from './component/yugioh/yugioh-list/yugioh-list/yugioh-list.component';
+import {YugiohDetailsComponent} from './component/yugioh/yugioh-details/yugioh-details/yugioh-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'pokemon',
+    children: [
+      {
+        path:'',
+        component: PokemonListComponent
+      },
+      {
+        path:'monster',
+        component: PokemonDetailsComponent
+      },
+      {
+        path:'monster/:id',
+        component: PokemonDetailsComponent,
+        runGuardsAndResolvers: 'paramsChange'
+      }
+    ]
+
+  },
+  {
+    path: 'yugioh',
+    children: [
+      {
+        path: '',
+        component: YugiohListComponent
+      },
+      {
+        path: 'monster',
+        component: YugiohDetailsComponent
+      },
+      {
+        path: 'monster/:id',
+        component: YugiohDetailsComponent,
+        runGuardsAndResolvers: 'paramsChange'
+      }
+    ]
+  },
+];
